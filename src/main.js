71e498d423848a70b85a98095ef9093d99e4c564
Lovia <lcy980517@163.com>
2019-03-10 16:44:46 +0800

    2019.3.10 16:44

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// openlayers 的样式库
+import 'ol/ol.css';
+// ol-ext 的样式库
+import 'ol-ext/dist/ol-ext.min.css';
+
+
+// 加载 Element 配置
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI, {
+  size: 'small'
+});
+
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
